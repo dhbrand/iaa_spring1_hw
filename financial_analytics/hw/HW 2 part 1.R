7a0e54a9d8724b63317297ccc2ac657817c5e6d9
wjjenista <42449580+wjjenista@users.noreply.github.com>
2019-02-17 17:37:17 -0500

    Update HW 2 part 1.R

--- financial_analytics/hw/HW 2 part 1.R
+++ financial_analytics/hw/HW 2 part 1.R
@@ -20,66 +20,69 @@ DJIA = c("MMM","AXP","AAPL","BA","CAT","CVX","CSCO","KO","DWDP","XOM","GS","HD",
          "JPM","MCD","MRK","MSFT","NKE","PFE","PG","TRV","UNH","UTX","VZ","V","WMT","WBA","DIS")
 
 
-getSymbols(DJIA)
+# getSymbols(DJIA)
+# 
+# stocks <- cbind(MMM[,4],AXP[,4],AAPL[,4],BA[,4],CAT[,4],CVX[,4],CSCO[,4],KO[,4],DWDP[,4],XOM[,4],GS[,4],HD[,4],
+#       IBM[,4],INTC[,4],JNJ[,4],JPM[,4],MCD[,4],MRK[,4],MSFT[,4],NKE[,4],PFE[,4],PG[,4],TRV[,4],UNH[,4],UTX[,4],VZ[,4],V[,4],WMT[,4],WBA[,4],DIS[,4])
+# 
+# paste_names <- paste(DJIA,".returns",sep = '')
+# 
+# #Create Rolling returns for each stock
+# #Cbind the stock DF with the returns DF
+# 
+# iterations = 3049
+# variables = 30
+# 
+# output <- matrix(ncol=variables, nrow=iterations)
+# 
+# for(i in 1:variables){
+#   output[,i] <- ROC(stocks[,i])
+# }
+# 
+# stocks <- cbind(stocks,output)
+# 
+# stocks <- stocks["2017-02/2019-02-08"]
+# 
+# paste_names
+# 
+# colnames(stocks)[31:60] <- c(paste_names)
 
-stocks <- cbind(MMM[,4],AXP[,4],AAPL[,4],BA[,4],CAT[,4],CVX[,4],CSCO[,4],KO[,4],DWDP[,4],XOM[,4],GS[,4],HD[,4],
-      IBM[,4],INTC[,4],JNJ[,4],JPM[,4],MCD[,4],MRK[,4],MSFT[,4],NKE[,4],PFE[,4],PG[,4],TRV[,4],UNH[,4],UTX[,4],VZ[,4],V[,4],WMT[,4],WBA[,4],DIS[,4])
-
-paste_names <- paste(DJIA,".returns",sep = '')
-
-#Create Rolling returns for each stock
-#Cbind the stock DF with the returns DF
-
-iterations = 3049
-variables = 30
-
-output <- matrix(ncol=variables, nrow=iterations)
-
-for(i in 1:variables){
-  output[,i] <- ROC(stocks[,i])
-}
-
-stocks <- cbind(stocks,output)
-
-paste_names
-
-colnames(stocks)[31:60] <- c(paste_names)
+stocks <- readr::read_csv("GitHub/og_sp1_hw/financial_analytics/hw/Stocks.csv")
+stocks <- stocks[,2:61]
 
 # Part 1 #
 
 # Rank stocks by ARCH test significance
 ARCH_tests = list()
 for (i in 1:30){
-  ARCH_tests[DJIA[i]] = arch.test(arima(stocks[,i], order = c(0,0,0)), output = TRUE)[,4]
+  ARCH_tests[DJIA[i]] = arch.test(arima(stocks[,i+30], order = c(0,0,0)), output = TRUE)[,4]
 }
 warnings()
 tests <- data.frame(unlist(ARCH_tests), DJIA)
 tests <- arrange(tests,desc(unlist.ARCH_tests.))
 ## Top 5: WMT, JNJ, PG, IBM, NKE
 top5 = c("IBM.Close","JNJ.Close","NKE.Close","PG.Close","WMT.Close","ibm_r","jnj_r","nke_r","pg_r","wmt_r")
-stocks_df <- data.frame(stocks)
-top_stocks <- stocks[,top5]
 
 # Create GARCH models and get parameters and AIC for each stock
 
 info = data.frame()
-top = c("IBM", "JNJ", "NKE", "PG", "WMT")
-for (i in 1:5){
+
+for (i in top5[6:10]){
   
   temp <- data.frame()
   
   j <- 1
   # GARCH-Normal
-  GARCH.N <- garchFit(formula= ~ garch(1,1), data=stocks[,i+5],
+  GARCH.N <- garchFit(formula= ~ garch(1,1), data=stocks[,i],
                       cond.dist="norm", include.mean = FALSE)
-  temp[j,"model"] <- "GARCH-Normal"
-  temp[j,"alpha"] <- GARCH.N@fit$par["alpha1"]
-  temp[j,"beta"]  <- GARCH.N@fit$par["beta1"]
-  temp[j,"AIC"]   <- GARCH.N@fit$ics["AIC"]
+  temp[i,"model"] <- "GARCH-Normal"
+  temp[i,"alpha"] <- GARCH.N@fit$par["alpha1"]
+  temp[i,"beta"]  <- GARCH.N@fit$par["beta1"]
+  temp[i,"AIC"]   <- GARCH.N@fit$ics["AIC"]
   
   j <- 2
   # GARCH-t
-  GARCH.t <- garchFit(formula= ~ garch(1,1), data=stocks[,i+5], 
+  GARCH.t <- garchFit(formula= ~ garch(1,1), data=stocks[,i], 
                       cond.dist="std", include.mean = FALSE)
   temp[j,"model"] <- "GARCH-t"
   temp[j,"alpha"] <- GARCH.t@fit$par["alpha1"]
@@ -88,7 +91,7 @@ for (i in 1:5){
   
   j <- 3
   # QGARCH-Normal
-  Skew.GARCH.N <- garchFit(formula= ~ garch(1,1), data=stocks[,i+5], 
+  Skew.GARCH.N <- garchFit(formula= ~ garch(1,1), data=stocks[,i], 
                            cond.dist="snorm",include.mean = FALSE)
   temp[j,"model"] <- "QGARCH-Normal"
   temp[j,"alpha"] <- Skew.GARCH.N@fit$par["alpha1"]
@@ -97,7 +100,7 @@ for (i in 1:5){
   
   j <- 4
   # QGARCH-t
-  Skew.GARCH.t <- garchFit(formula= ~ garch(1,1), data=stocks[,i+5], 
+  Skew.GARCH.t <- garchFit(formula= ~ garch(1,1), data=stocks[,i], 
                            cond.dist="sstd", include.mean = FALSE)
   temp[j,"model"] <- "QGARCH-t"
   temp[j,"alpha"] <- Skew.GARCH.t@fit$par["alpha1"]
@@ -108,14 +111,15 @@ for (i in 1:5){
 }
 
 # best models
-info <- info[c(2,8,10,14,20),]
-info["stock"] <- top
+best_GARCH_models <- info[c(2,8,10,14,20),]
+top <- c("IBM", "JNJ", "NKE","PG", "WMT")
+best_GARCH_models["stock"] <- top
 
 # rank by alpha (most susceptible to market shock)
-info <- arrange(info, desc(alpha))
+rank_alpha <- arrange(best_GARCH_models, desc(alpha))
 ## from most to least susceptible: NKE, WMT, JNJ, IBM, PG
 
 # rank by beta (longest lasting shock effects)
-info <- arrange(info, desc(beta))
+rank_beta <- arrange(best_GARCH_models, desc(beta))
 ## from longest to shortest effect: PG, IBM, JNJ, NKE, WMT
 
